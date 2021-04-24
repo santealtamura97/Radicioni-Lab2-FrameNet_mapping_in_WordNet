3a7f000086751a0b4cf75e4bc101dc42caf15d03
Sante <sante_altamura97@hotmail.com>
2021-04-24 19:09:10 +0200

    modifiche

--- main.py
+++ main.py
@@ -10,16 +10,73 @@ from nltk.corpus import framenet as fn
 from nltk.corpus import wordnet as wn
 import utils
 
+SURNAME = 'Altamura'
 
-"""
-frame_name = "scrutizing_for"
-frame_name = frame_name.replace("_"," ")
-tokens = nltk.word_tokenize(frame_name)
-print("Tokens: ",tokens)
-print("Pos tag: ",nltk.pos_tag(tokens))
-"""
+def get_contexts_frames_list(frames):
+    contexts_frames_list = []
+    for frame in frames:
+      
+        #dati riguardanti i frame elements e le lexical units del frame
+        frame_elements_contexts = dict()
+        lexical_units_contexts = dict()
+        
+        FEs = frame.FE.keys()
+        for fe in FEs:
+            fed = frame.FE[fe]
+            frame_elements_contexts[fed.name] = utils.context_for_frame_component(fed)
+            
+        LUs = frame.lexUnit.keys()
+        for lu in LUs:
+            lud = frame.lexUnit[lu]
+            lexical_units_contexts[lud.name] = utils.context_for_frame_component(lud)
+        
+        contextsFrame = utils.ContextsFrame(frame.ID, frame.name, utils.context_for_frame(frame), 
+                                            frame_elements_contexts, lexical_units_contexts)
+        contexts_frames_list.append(contextsFrame) 
+    return contexts_frames_list
 
-#print(fn.frames('Process_stopped_state')[0])
-print(utils.context_for_frame(fn.frame_by_name('Cache')))
-print()
-print(utils.context_for_sense(wn.synsets('Cache')[0]))
+#restituisce un senso di WordNet per il frameNet_name(frame name, frame element name, lexical unit name)
+#che massimizza lo score
+def compute_score(frameNet_name, frameNet_context):
+    wordnet_name = utils.get_regent(frameNet_name)
+    synsets = wn.synsets(wordnet_name)
+    if synsets == []:
+        return None
+    #prende il synset con lo score più alto
+    max_score = 0
+    best_synset = synsets[0]
+    for synset in synsets:
+        synset_context = utils.context_for_sense(synset)
+        score = get_score(frameNet_context,synset_context)
+        if score > max_score:
+            max_score = score
+            best_synset = synset
+    return best_synset
+
+def get_score(context1,context2):
+    return len(context1.intersection(context2)) + 1
+    
+
+def get_synsets_frames_list(contexts_frame_list):
+    for contextsFrame in contexts_frame_list:
+        frame_synset = compute_score(contextsFrame.get_frame_name(),
+                                     contextsFrame.get_frame_context())
+        frame_elements_synsets = dict()
+        #lexical_units_synsets = dict()
+        frame_elements_contexts = contextsFrame.get_frame_elements_contexts()
+        for frame_element_name in frame_elements_contexts:
+            score = compute_score(frame_element_name, frame_elements_contexts[frame_element_name])
+            if not type(score) == None:
+               frame_elements_synsets[frame_element_name] = score
+               
+        #test
+        print("=====================")
+        print(frame_synset)
+        print(frame_elements_synsets)
+        
+def main():
+    #Lista di oggetti ti tipo ContextsFrame
+    contexts_frames_list = get_contexts_frames_list(utils.getFrameSetForStudent(SURNAME))
+    get_synsets_frames_list(contexts_frames_list)
+    
+main()
--- utils.py
+++ utils.py
@@ -8,7 +8,99 @@ Created on Sat Apr 17 11:24:19 2021
 import string
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
+import hashlib
+import random
+from random import randint
+from random import seed
+from nltk.corpus import framenet as fn
+import spacy
+from nltk.corpus import wordnet as wn
+import nltk
+#spacy.cli.download("en_core_web_sm")
 
+#Contesti associati ad un Frame. Struttura dati che contiene
+#i contesti associati al frame, ai suoi frame elements e alle sue
+#lexical units
+class ContextsFrame:
+    
+    def __init__(self, frame_id, frame_name, frame_context,
+                 frame_elements_contexts, lexical_units_contexts):
+        self.frame_id = frame_id
+        self.frame_name = frame_name
+        self.frame_context = frame_context
+        #dizionario: [frame_element_name] -> context
+        self.frame_elements_contexts = frame_elements_contexts
+        
+        #dizionario: [lexical_unit_name] -> context
+        self.lexical_units_contexts = lexical_units_contexts
+        
+    def get_frame_elements_contexts(self):
+        return self.frame_elements_contexts
+    def get_lexical_units_contexts(self):
+        return self.lexical_units_contexts
+    def get_frame_id(self):
+        return self.frame_id
+    def get_frame_name(self):
+        return self.frame_name
+    def get_frame_context(self):
+        return self.frame_context
+    #stampa fatta al solo scopo di conoscere le struttura interna della classe
+    def printContextsFrame(self):
+        print("FRAME ID: ", self.get_frame_id())
+        print("FRAME NAME: ",self.get_frame_name())
+        print("\nFRAME CONTEXT: ","\n",self.get_frame_context())
+        print("\nFRAME ELEMENTS CONTEXTS: ","\n", self.get_frame_elements_contexts())
+        print("\nLEXICAL UNTIS CONTEXTS: ","\n", self.get_lexical_units_contexts())
+        
+    
+#Risultati di WordNet.
+#Struttura che contiene i synset associati al frame in questione
+#ai suoi frame elements e alle sue lexical_units
+#Questi risultati dovranno poi essere confrontati con 
+#annotazioni fatte degli umani
+#quindi ogni annotazione per ogni frame dovrà essere di questo tipo
+#e.s SynsetsFrameAnnotation
+class SynsetsFrame:
+    def __init__(self, frame_id, frame_name, frame_synset,
+                 frame_elements_synsets, lexical_units_synsets):
+        self.frame_id = frame_id
+        self.frame_name = frame_name
+        self.frame_synset = frame_synset
+        self.frame_elements_synsets = frame_elements_synsets
+        self.lexical_units_synsets = lexical_units_synsets
+
+def print_frames_with_IDs():
+    for x in fn.frames():
+        print('{}\t{}'.format(x.ID, x.name))
+
+def get_frams_IDs():
+    return [f.ID for f in fn.frames()]   
+
+#restituisce un insieme di 5 frame legati allo studente in input ('Altamura')
+def getFrameSetForStudent(surname, list_len=5):
+    nof_frames = len(fn.frames())
+    base_idx = (abs(int(hashlib.sha512(surname.encode('utf-8')).hexdigest(), 16)) % nof_frames)
+    print('\nstudent: ' + surname + "\n")
+    framenet_IDs = get_frams_IDs()
+    i = 0
+    offset = 0 
+    seed(1)
+    frame_list = []
+    while i < list_len:
+        fID = framenet_IDs[(base_idx+offset)%nof_frames]
+        f = fn.frame(fID)
+        frame_list.append(fn.frame(fID))
+        fNAME = f.name
+        #print('\tID: {a:4d}\tframe: {framename}'.format(a=fID, framename=fNAME))
+        offset = randint(0, nof_frames)
+        i += 1
+    return frame_list        
+
+#Prende in input un frame di FrameNet e restituisce il suo contesto
+#formato da tutte le definizioni dei suoi frame element, lexical unit
+#e del frame stesso. Le frasi sono tutte pre-processate
+#in modo tale da ottenere un set di parole lemmatizzate, senza stopwords
+#senza punteggiatura
 def context_for_frame(frame):
     context_frame = set()
     
@@ -26,6 +118,21 @@ def context_for_frame(frame):
     
     return context_frame
 
+#Restituisce il contesto per un frame component (frame element o lexical unit)
+#che è praticamente formato dalla sua definizione, lemmatizzata,
+#da cui sono state rimosse le stop words e punteggiatura
+def context_for_frame_component(frame_component):
+    context_frame_component = set()
+    
+    context_frame_component.update(pre_processing(frame_component.definition))
+    
+    return context_frame_component
+
+#Prende in input un senso di WordNet e restituisce il suo contesto
+#formato da tutte le definizioni ed esempi dei suoi iperonimi, iponimi
+# e definizione ed esempi del senso stesso. Le frasi sono tutte pre-processate
+#in modo tale da ottenere un set di parole lemmatizzate, senza stopwords
+#senza punteggiatura
 def context_for_sense(sense):
     context_sense = set()
     
@@ -43,7 +150,37 @@ def context_for_sense(sense):
         for example in hyponym.examples():
             context_sense.update(pre_processing(example))
     return context_sense
-        
+
+#riceve in input una frase semplice(costituita da circa due parole separate da _)
+#e restituisce in output il reggente della frase
+def get_regent(sentence):
+    nlp = spacy.load("en_core_web_sm")
+    #nlp = en_core_web_sm.load()
+    if("_" in sentence or "-" in sentence):            
+        sentence = sentence.replace("_", " ")
+        sentence = sentence.replace("-", " ")
+         #se la multiword ha un senso in WordNet, non è necessario individuare il reggente dell'espressione
+        if(wn.synsets(sentence) == []):
+            doc = nlp(sentence)
+            chunks = list(doc.noun_chunks)
+            try:
+                chunk = chunks[0]
+                regent = chunk.root.text
+            except IndexError:
+                regent = sentence.split(" ")[0]
+        else:
+             regent = sentence
+    else:
+        regent = sentence
+    return regent
+
+#Le unità lessicali ricavate da FrameNet per un determinato frame sono nella forma <ul>.PoS (esempio: before.prep)
+#pertanto, per poter individuare il senso o i sensi corrispondenti all'unità lessicale, è importante rimuovere
+#il punto e il PoS che segue
+def remove_pos_lu(lexical_unit_name):
+    new_lexical_unit_name = lexical_unit_name.split(".")[0]
+    return new_lexical_unit_name
+    
 
 #Rimuove le stopwords da una lista di parola
 def remove_stopwords(words_list):
@@ -78,9 +215,23 @@ def get_stopwords():
 def tokenize_sentence(sentence):
     words_list = []
     lmtzr = WordNetLemmatizer()
+    for tag in nltk.pos_tag(word_tokenize(sentence)):
+        if (tag[1][:2] == "NN"):
+            words_list.append(lmtzr.lemmatize(tag[0], pos = wn.NOUN))
+        elif (tag[1][:2] == "VB"):
+             words_list.append(lmtzr.lemmatize(tag[0], pos = wn.VERB))
+        elif (tag[1][:2] == "RB"):
+             words_list.append(lmtzr.lemmatize(tag[0], pos = wn.ADV))
+        elif (tag[1][:2] == "JJ"):
+             words_list.append(lmtzr.lemmatize(tag[0], pos = wn.ADJ))
+    return words_list
+    """
+    words_list = []
+    lmtzr = WordNetLemmatizer()
     for word in word_tokenize(sentence):
         words_list.append(lmtzr.lemmatize(word))
-    return words_list
+    return words_list"""
+    
 
 def pre_processing(sentence):
     return remove_stopwords(remove_punctuation(tokenize_sentence(sentence)))
